fc31620ed357eeced5c97d4be3c9f8b7f135437d
Scott Robertson (RIT Student) <smr7178@ad.rit.edu>
2017-10-13 11:12:24 -0400

    started rtd

---
+++ Fungus Otaku kara hiro made/Assets/Scripts/RTD/RTDmanager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTDmanager : MonoBehaviour {
+	public List<GameObject> nodes;
+	private List<GameObject> soldiers;
+
+	// Use this for initialization
+	void Start () {
+		nodes = new List<GameObject> ();
+		soldiers = new List<GameObject> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Fungus Otaku kara hiro made/Assets/Scripts/RTD/Soldier.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soldier : MonoBehaviour {
+	private int hp;
+	private float speed;
+	private int targetNodeIndex;
+	private RTDmanager rtdmanager;
+
+	public int HP{get{return hp; }}
+
+	// Use this for initialization
+	void Start () {
+		targetNodeIndex = 0;
+		rtdmanager = GameObject.FindGameObjectWithTag ("RTDmanager").GetComponent<RTDmanager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Move ()
+	{
+		//move towards node
+		//if in range of target, change target to the next node
+	}
+
+	void TakeDamage(int damage)
+	{
+		hp -= damage;
+
+		if(hp < 0)
+		{
+			//die
+		}
+	}
+}
